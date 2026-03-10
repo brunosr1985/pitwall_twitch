f5ae5ea9d264a50ffe9fa0628ec89afec324bdf8
Bruno dos Santos Rodrigues <brunosr1985@live.com>
2026-03-10 11:04:31 -0300

    Create secrets.js

---
+++ secrets.js
@@ -0,0 +1,11 @@
+        const myChannel = "";
+        const teamNameTwitch = ""; 
+        
+        // Suas credenciais da Twitch (já preenchidas com as que você forneceu)
+        const helixClientID = ""; 
+        const helixSecret = ""; 
+        
+        // A sua lista de segurança (Pneu de Chuva)
+        const backupTeam = []; 
+        
+        let concentraTeam = [];
